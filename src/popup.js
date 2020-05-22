abef0b7e89ac9430f2ff2e2663fa411d380a705a
Thomas Lovén <thomasloven@gmail.com>
2020-05-22 09:17:24 +0200

    Fix popup icons for 0.100. Closes #2. Thanks @InduPrakash

--- src/popup.js
+++ src/popup.js
@@ -39,10 +39,11 @@ export function popUp(title, card, large=false, style=null, fullscreen=false) {
     ? ``
     : `
       <app-toolbar>
-        <paper-icon-button
+        <ha-icon-button
           icon="hass:close"
           dialog-dismiss=""
-        ></paper-icon-button>
+          aria-label="Dismiss dialog"
+        ></ha-icon-button>
         <div class="main-title" main-title="">
           ${title}
         </div>
