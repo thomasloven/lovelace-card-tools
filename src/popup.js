a6c0ee7eed6347febc8e0596a5a03c6b174d4cbc
Dwain Scheeren <info@dwain.nl>
2022-09-16 15:41:43 +0200

    Update popup.js
    Firevent needs to be removed because it opens a empty popup with . entity.

--- src/popup.js
+++ src/popup.js
@@ -5,7 +5,6 @@ import "./lovelace-element";
 
 export async function closePopUp() {
   const root = document.querySelector("home-assistant") || document.querySelector("hc-root");
-  fireEvent("hass-more-info", {entityId: "."}, root);
   const el = await selectTree(root, "$ card-tools-popup");
 
   if(el)
